0480bb1c1585a04e417b6f9f3793461c5edb85b2
Leonardorossato <leonardorossato21@gmail.com>
2023-02-07 13:43:57 -0300

    feat: create task.controller.js

---
+++ src/controllers/task.controller.js
@@ -0,0 +1,22 @@
+class TaskController {
+    static create = async(req, res, next) =>{
+
+    }
+    static findAll = async(req, res) =>{
+
+    }
+
+    static findOne = async(req, res) =>{
+
+    }
+
+    static updated =async(req, res) =>{
+
+    }
+
+    static delete = async(req, res) =>{
+
+    }
+}
+
+module.exports = TaskController;
