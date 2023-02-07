cbf144390d2b99fb5d402d62da441cc3d621aec1
Leonardorossato <leonardorossato21@gmail.com>
2023-02-07 13:51:15 -0300

    update task.schema.js

--- src/schemas/task.schema.js
+++ src/schemas/task.schema.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
   name: { type: String, required: true, minlength: 3, maxlength: 200 },
-  completed: { type: Boolean },
+  completed: { type: Boolean.apply, default: false },
 });
 
 const Task = mongoose.model("Task", TaskSchema);
