88843b649d7d1f810efaeb4038ab514b6799cfce
Leonardorossato <leonardorossato21@gmail.com>
2023-02-07 14:01:33 -0300

    update server.js

--- src/server.js
+++ src/server.js
@@ -3,8 +3,11 @@ const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT;
 const mongoConnection = require("./config/mongo.config");
+const taksRouter = require("./routes/task.route");
 
 app.mongoConnection = mongoConnection;
+
+app.use("/api/tasks", taksRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
