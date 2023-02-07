db9e292495fd94235ab4c4ef91af7091b2ce1abb
Leonardorossato <leonardorossato21@gmail.com>
2023-02-07 13:26:08 -0300

    update server.js

---
+++ src/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+require('dotenv').config()
+const PORT = process.env.PORT
+
+app.listen(PORT, () =>{
+    console.log(`Server is listening on port ${PORT}`)
+})
