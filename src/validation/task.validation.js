4aa7b93f7cc752b621dcd3e46caacb8ba13608dc
Leonardorossato <leonardorossato21@gmail.com>
2023-02-07 15:09:42 -0300

    update task.validation.js

--- src/validation/task.validation.js
+++ src/validation/task.validation.js
@@ -1,6 +1,10 @@
+const Joi = require("joi");
+
 const validationTasks = (tasks) => {
   const schema = Joi.object({
     name: Joi.string().min(5).max(100).required(),
   });
   return schema.validate(tasks);
 };
+
+module.exports= validationTasks
