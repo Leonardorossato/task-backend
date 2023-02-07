a730f45b09910f43a9f212f7acca4c7fcf590cdf
Leonardorossato <leonardorossato21@gmail.com>
2023-02-07 13:31:05 -0300

    update server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config()
-const PORT = process.env.PORT
+require("dotenv").config();
+const PORT = process.env.PORT;
+const mongoConnection = require("./config/mongo.config");
 
-app.listen(PORT, () =>{
-    console.log(`Server is listening on port ${PORT}`)
-})
+app.mongoConnection = mongoConnection;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
